bcb6abf85146b18c4b7326d3e9e73f168e0be6c8
sincollmm <56375360+sincollmm@users.noreply.github.com>
2022-05-22 21:43:29 +0700

    finish need change database

--- BlazorAppQLDT/Server/Data/DataContext.cs
+++ BlazorAppQLDT/Server/Data/DataContext.cs
@@ -6,19 +6,7 @@
         {
 
         }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Comic>().HasData(
-                new Comic { Id = 1, Name ="Marvel"},
-                new Comic { Id = 2, Name ="DC"}
-                );
-            modelBuilder.Entity<SuperHero>().HasData(
-                new SuperHero { Id = 1, FirstName ="Peter",LastName ="Parker",HeroName ="Spideman", ComicId = 1},
-                new SuperHero { Id = 2, FirstName ="Bruce",LastName = "Wayne", HeroName ="Batman", ComicId= 2}
-                );
-        }
-        public DbSet<SuperHero> SuperHeroes { get; set; }
-        public DbSet<Comic> Comics { get; set; }
+        
         public DbSet<SinhvienModel> Sinhviens { get; set; }
     }
 }
